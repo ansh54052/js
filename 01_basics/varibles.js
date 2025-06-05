b85252964587568b90150532c9cac361e7eb384a
ansh54052 <ansh54052@gmail.com>
2025-06-05 15:29:47 +0000

    variables

---
+++ 01_basics/varibles.js
@@ -0,0 +1,6 @@
+const accountID="121"
+let accountEMAIL="ANSH.COM"
+var city = 'orai'
+var password =21212
+console.table([accountEMAIL,accountID,city,password])
+// use let instead of var because of block scope and functional scope
