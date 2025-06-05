e409c05f0298b8ba18c1c87f7e24bb0e8f4e0837
ansh54052 <ansh54052@gmail.com>
2025-06-05 15:26:37 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// treat all js code in new version
+// alert(3+1) not work since we are using node not browser
+console.log(1+1);
+let name = "ansh"
+let age = 19 
+let isloggedin = false
+
+//number => 2 to power 53
+//big int 
+//string => ""
+// boolean=>true/false
+//null => stand alone value 
+//undefined => value assign nai hui
+//symbol => for uniqueness
+
+// object
+
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof null)
+console.log(typeof undefined)
