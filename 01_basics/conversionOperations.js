e29fbe62f5932144103126bb81dd294c6613c372
ansh54052 <ansh54052@gmail.com>
2025-06-18 14:03:27 +0000

    coversionoperations

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -11,14 +11,43 @@
 // "33abc" => nAn
 // true => 1 , false => 0
 
-let isloggedin = "a"
-let booleanISloggedIn = Boolean(isloggedin)
-console.log(booleanISloggedIn)
+// let isloggedin = "a"
+// let booleanISloggedIn = Boolean(isloggedin)
+// console.log(booleanISloggedIn)
 
 
 // 1=> true ,0=>false
 //""=>(gave) false, "ansh"=>true
 
-let someno = 33
-let stringnumber = String(someno)
-console.log(typeof stringnumber)
+// let someno = 33
+// let stringnumber = String(someno)
+// console.log(typeof stringnumber)
+
+// let value = 3 
+// let negval= -value
+// console.log(negval)
+
+// console.log(2%1)
+// console.log(2-1)
+// console.log(2+1)
+// console.log(2*3)
+
+// let str1 = "hello"
+// let str2 = " ansh"
+// let str3 = str1 + str2 
+// console.log(str3)
+
+// console.log("1"+2)  //12
+// console.log(1+"2")  //12
+// console.log("1"+"2")  //12
+// console.log("1"+2+2) //=>122  , pehle agar string hai to phr poore o string ki tarah consider kiya jata hai
+// console.log(1+2+"2")//=>32  , last main agar string hai to pehle normal maths chalti hai   // mre on ecma
+// console.log((1+3)*3%2) 
+
+
+// console.log(+true)  
+// console.log(+"")  
+
+// let num1,num2,num3
+// num1=num2=num3=2+2
+// console.log(num2)
