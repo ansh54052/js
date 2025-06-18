ee619cdee42788c003208395d221916917b8d0f8
ansh54052 <ansh54052@gmail.com>
2025-06-18 13:39:13 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,24 @@
+// let score = undefined 
+// console.log(typeof(score))
+// console.log(typeof score)
+// let valueinnumber = Number(score)
+// console.log (valueinnumber)
+
+
+//notes:-
+
+// "33" easily convert into no. 33
+// "33abc" => nAn
+// true => 1 , false => 0
+
+let isloggedin = "a"
+let booleanISloggedIn = Boolean(isloggedin)
+console.log(booleanISloggedIn)
+
+
+// 1=> true ,0=>false
+//""=>(gave) false, "ansh"=>true
+
+let someno = 33
+let stringnumber = String(someno)
+console.log(typeof stringnumber)
