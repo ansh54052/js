27e37ed8e5bebf8cbbdb2dc9b58d9a10d108d03d
ansh54052 <ansh54052@gmail.com>
2025-06-20 14:54:21 +0000

    datatype summary

--- 01_basics/datatype_summary.js
+++ 01_basics/datatype_summary.js
@@ -35,3 +35,8 @@ const myFunction = function(){
 console.log(typeof anotherId);
 
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//memory
+
+// primitive data used stack(means koi bhi cheez ki copy banti hai and agar koi chaneg karenge to copy main hoga na ki actual thing pr)
+// non primitive used heap ( means koi vhi value ka reference jata hai)
